c243b0429d31a0446985c154132fa3b546a15118
Jennifer Schweers <orange.jenny@gmail.com>
2016-07-21 15:02:16 -0400

    initial interactions

---
+++ cm_prototype/static/main.js
@@ -0,0 +1,37 @@
+      var templates = {
+        form: "<div class='form'><input /><ol></ol><button class='add-question'>Add question</button></div>",
+        question: "<li><input /><a class='remove-question' href='javascript:void(0)'>x</a></li>",
+        workflow: "<div class='workflow'><div class='start'></div><div class='middle'></div></div>",
+      };
+      var dropForm = function(event, ui) {
+        var $form = $(ui.draggable);
+        $form.css("top", 0);
+        $form.css("left", 0);
+        $(this).append($form);
+      };
+      $(document).ready(function() {
+        $(".toolbar-icon-form").click(function() {
+          var $form = $(templates.form);
+          $form.find("input").val("Form " + ($(".form").length + 1));
+          $form.draggable({ revert: true });
+          $("#workspace").append($form);
+        });
+        $(".toolbar-icon-workflow").click(function() {
+          var $workflow = $(templates.workflow);
+          $workflow.find(".middle").droppable({
+            accept: ".form",
+            drop: dropForm,
+          });
+          $workflow.find(".start").droppable({
+            accept: ".form",
+            drop: dropForm,
+          });
+          $("#workspace").append($workflow);
+        });
+        $(document).on("click", ".add-question", function() {
+          $(this).closest(".form").find("ol").append($(templates.question));
+        });
+        $(document).on("click", ".remove-question", function() {
+          $(this).closest("li").remove();
+        });
+      });
