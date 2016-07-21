74d797015c6321e11b74a6ace427d75f0960f3a7
Biyeun Buczyk <biyeun@dimagi.com>
2016-07-21 14:34:15 -0400

    add compiled sass

--- cm_prototype/assets.py
+++ cm_prototype/assets.py
@@ -3,6 +3,7 @@ from flask_assets import Bundle
 # todo serve from Amazon S3
 common_css = Bundle(
     'components/font-awesome/css/font-awesome.min.css',
+    'components/tether/dist/css/tether.min.css',
     'components/bootstrap/dist/css/bootstrap.min.css',
     Bundle(
         'sass/style.scss',
@@ -15,6 +16,7 @@ common_js = Bundle(
     'components/jquery/dist/jquery.min.js',
     'components/knockout/dist/knockout.js',
     'components/underscore/underscore-min.js',
+    'components/tether/dist/js/tether.min.js',
     'components/bootstrap/dist/js/bootstrap.js',
     output='public/js/common.js'
 )
