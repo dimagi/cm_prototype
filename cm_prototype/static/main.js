70d02a27e5daea057a655c6290afdddd772b6c02
Jennifer Schweers <orange.jenny@gmail.com>
2016-07-21 15:27:23 -0400

    add help on hover

--- cm_prototype/static/main.js
+++ cm_prototype/static/main.js
@@ -1,5 +1,5 @@
       var templates = {
-        form: "<div class='form'><input /><ol></ol><button class='add-question'>Add question</button></div>",
+        form: "<div class='form'><input class='name' /><ol></ol><button class='add-question'>Add question</button></div>",
         question: "<li><input /><a class='remove-question' href='javascript:void(0)'>x</a></li>",
         workflow: "<div class='workflow'><div class='start'><span class='title'>Start Form</span></div><div class='middle'><span class='title'>Other Forms</span></div></div>",
       };
@@ -9,12 +9,38 @@
         $form.css("left", 0);
         $(this).append($form);
       };
+      var clearHelp = function() {
+        $("#help").text("");
+        $("#help").css("display", "none");
+      };
+      var setHelp = function(text) {
+        $("#help").text(text);
+        $("#help").css("display", "block");
+      };
       $(document).ready(function() {
         $(".toolbar-icon-form").click(function() {
           var $form = $(templates.form);
           $form.find("input").val("Form " + ($(".form").length + 1));
           $form.draggable({ revert: true });
           $("#workspace").append($form);
+        });
+        $(".toolbar-icon-form").hover(function() {
+            setHelp("[form help]");
+        }, function() {
+            clearHelp();
+        });
+        $(".toolbar-icon-workflow").hover(function() {
+            setHelp("[workflow help]");
+        }, function() {
+            clearHelp();
+        });
+        $(document).on('mouseover', '.workflow .start', function() {
+            if (!$(this).find(".form").length) {
+                setHelp("[start help]");
+            }
+        });
+        $(document).on('mouseout', '.workflow .start', function() {
+            clearHelp();
         });
         $(".toolbar-icon-workflow").click(function() {
           var $workflow = $(templates.workflow);
@@ -33,5 +59,12 @@
         });
         $(document).on("click", ".remove-question", function() {
           $(this).closest("li").remove();
+        });
+        var $help = $("#help");
+        $(document).on("mousemove", function(e) {
+            if ($help.text().trim()) {
+                $help.css("top", e.pageY + 10);
+                $help.css("left", e.pageX + 10);
+            }
         });
       });
